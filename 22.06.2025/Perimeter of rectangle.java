e1a0c7ccb26c7a43f3374a3843d8f5ec9d8026b9
Preethi2206 <147687615+Preethi2206@users.noreply.github.com>
2025-06-23 09:24:00 +0530

    Perimeter of rectangle.java

---
+++ 22.06.2025/Perimeter of rectangle.java
@@ -0,0 +1,7 @@
+public class RectanglePerimeter {
+    public static void main(String[] args) {
+        int length = 5, width = 4;
+        int perimeter = 2 * (length + width);
+        System.out.println("Perimeter: " + perimeter);
+    }
+}
