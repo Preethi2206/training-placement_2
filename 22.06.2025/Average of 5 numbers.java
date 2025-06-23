5665f851166ce101117031d38f9b77fca4ffe3a2
Preethi2206 <147687615+Preethi2206@users.noreply.github.com>
2025-06-23 09:24:51 +0530

    Average of 5 numbers.java

---
+++ 22.06.2025/Average of 5 numbers.java
@@ -0,0 +1,7 @@
+public class Average {
+    public static void main(String[] args) {
+        int a = 10, b = 20, c = 30, d = 40, e = 50;
+        double average = (a + b + c + d + e) / 5.0;
+        System.out.println("Average: " + average);
+    }
+}
