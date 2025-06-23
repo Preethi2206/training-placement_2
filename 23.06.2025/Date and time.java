3aa2a7e8ce646544458687688a70b4498caaed06
Preethi2206 <147687615+Preethi2206@users.noreply.github.com>
2025-06-23 18:49:17 +0530

    Date and time.java

---
+++ 23.06.2025/Date and time.java
@@ -0,0 +1,7 @@
+import java.time.LocalDateTime;
+public class DateTime {
+    public static void main(String[] args) {
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println("Current Date and Time: " + now);
+    }
+}
