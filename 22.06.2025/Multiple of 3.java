a218c6ea3255bb2ea822416775bc26b6bc34d496
Preethi2206 <147687615+Preethi2206@users.noreply.github.com>
2025-06-23 09:28:02 +0530

    Multiple of 3.java

---
+++ 22.06.2025/Multiple of 3.java
@@ -0,0 +1,9 @@
+public class MultipleOfThree {
+    public static void main(String[] args) {
+        int number = 9;
+        if (number % 3 == 0)
+            System.out.println(number + " is a multiple of 3");
+        else
+            System.out.println(number + " is not a multiple of 3");
+    }
+}
