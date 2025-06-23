80c50d51c60bc55954e40732063604b306372a88
Preethi2206 <147687615+Preethi2206@users.noreply.github.com>
2025-06-23 18:46:57 +0530

    Square root.java

---
+++ 23.06.2025/Square root.java
@@ -0,0 +1,7 @@
+public class SquareRoot {
+    public static void main(String[] args) {
+        int number = 25;
+        double sqrt = Math.sqrt(number);
+        System.out.println("Square root: " + sqrt);
+    }
+}
