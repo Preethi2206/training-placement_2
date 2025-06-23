f1511165836bf3ce012bbbf3b660ee1b185d90af
Preethi2206 <147687615+Preethi2206@users.noreply.github.com>
2025-06-23 09:22:28 +0530

    Word count.java

---
+++ 22.06.2025/Word count.java
@@ -0,0 +1,7 @@
+public class WordCount {
+    public static void main(String[] args) {
+        String str = "Java is fun";
+        String[] words = str.split(" ");
+        System.out.println("Word count: " + words.length);
+    }
+}
