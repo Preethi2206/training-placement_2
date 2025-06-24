384372af22d2fa48b4e4651d26fc11142afe689f
Preethi2206 <147687615+Preethi2206@users.noreply.github.com>
2025-06-24 18:57:55 +0530

    Factor of number.java

---
+++ 24.06.2025/Factor of number.java
@@ -0,0 +1,10 @@
+public class Factors {
+    public static void main(String[] args) {
+        int num = 12;
+        System.out.print("Factors of " + num + ": ");
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0)
+                System.out.print(i + " ");
+        }
+    }
+}
