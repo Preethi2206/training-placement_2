0cfbad29e498a9feb60750af63f66a23938b1b5e
Preethi2206 <147687615+Preethi2206@users.noreply.github.com>
2025-06-24 18:55:58 +0530

    Character is uppercase.java

---
+++ 24.06.2025/Character is uppercase.java
@@ -0,0 +1,9 @@
+public class CheckUpper {
+    public static void main(String[] args) {
+        char ch = 'G';
+        if (Character.isUpperCase(ch))
+            System.out.println("Uppercase letter");
+        else
+            System.out.println("Not uppercase");
+    }
+}
